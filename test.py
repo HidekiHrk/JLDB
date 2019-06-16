560faf5b47bc1da456f21b82b70b2afb4126ee15
HidekiHrk <hidekihiroki123@gmail.com>
2019-06-16 02:21:15 -0300

    Stable Table and Row system

--- test.py
+++
@@ -1,7 +0,0 @@
-import jldb
-
-client = jldb.Client('datinha.jldb')
-client.add_table('users', id=int, name=str, inventory=dict, coins=float)
-users = client.get_table('users')
-# users.add_row(id=0, name="hideki", inventory={'a':1}, coins=10)
-print(users.rows[1])
