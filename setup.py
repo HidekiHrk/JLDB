7e7b621b2cc2322e37f83d2660eaf83445fed6e0
HidekiHrk <hidekihiroki123@gmail.com>
2019-06-16 02:28:08 -0300

    .

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name='JLDB',
+  packages = ['jldb'],
+  version = '0.1',
+  license='MIT',
+  description = 'JSON-like db for python3.',
+  author = 'HidekiHrk',
+  author_email = 'hidekihiroki123@gmail.com',
+  url = 'https://github.com/HidekiHrk/JLDB',
+  download_url = 'https://github.com/HidekiHrk/PyDBQuery/archive/v_0.2.tar.gz',
+  keywords = ['python', 'db', 'database', 'better', 'query', 'json', 'dict'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
