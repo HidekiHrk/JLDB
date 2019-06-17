37928bdc3380e96dce49bdced972c3b949d4a227
HidekiHrk <hidekihiroki123@gmail.com>
2019-06-17 02:41:21 -0300

    .

--- jldb/__main__.py
+++ jldb/__main__.py
@@ -46,13 +46,54 @@ def c_confirm(iclass, value, raw=True):
         return value if raw else update_dict(iobj, value)
     return iclass(value)
 
+class Row(object):
+    def __init__(self, table, row_id: int):
+        super().__setattr__('table', table)
+        super().__setattr__('row_id', row_id)
+        super().__setattr__('_Row__column_types', self.table.columns)
+
+    @property
+    def __rowdict(self) -> dict:
+        return self.table.d_rows[self.row_id]
+
+    def __setattr__(self, name, value):
+        if self.__column_types.get(name) != None:
+            r_dict = self.__rowdict
+            r_class = self.__column_types[name]
+            r_dict[name] = c_confirm(r_class, value)
+            self.table.d_rows = set_key(self.table.d_rows, self.row_id, r_dict)
+        else:
+            return super().__setattr__(name, value)
+
+    def __getattr__(self, name):
+        if self.__rowdict.get(name) != None:
+            return c_confirm(self.__column_types[name], self.__rowdict.get(name), raw=False)
+
+    def __delattr__(self, name):
+        if self.__rowdict.get(name) != None:
+            self.table.d_rows = set_key(self.table.d_rows, self.row_id, del_key(self.__rowdict, name))
+        else:
+            return super().__delattr__(name)
+
+    def delete(self):
+        self.table.remove_row(self.row_id)
+
+    @property
+    def dict(self) -> dict:
+        pdict = self.__dict__
+        for key in self.__rowdict:
+            value = self.__rowdict.get(key)
+            clss = self.__column_types.get(key)
+            pdict[key] = c_confirm(clss, value, raw=False)
+        return pdict
+
 class Table:
     def __init__(self, client, table_id: str):
         self.client = client
         self.__id = table_id
 
     @property
-    def id(self):
+    def id(self) -> str:
         if not self.__id:
             raise TableNotFoundError("This table does not exists anymore, please delete the ref to this object in your code.")
         return self.__id
@@ -68,12 +109,12 @@ class Table:
         self.client.update_table(self.id, value)
 
     @property
-    def name(self):
+    def name(self) -> str:
         # Database Name
         return self._dict['name']
     
     @property
-    def columns(self):
+    def columns(self) -> dict:
         # Get database columns and types of each one
         cols = self._dict['columns']
         return dict(map(lambda x: [x, get_class_modules_from_str(cols[x])], cols))
@@ -99,7 +140,7 @@ class Table:
         # Get a list of Row objects that's stored on this table
         return list(map(lambda x: Row(self, x), self.d_rows))
     
-    def get_rows(self, **cols):
+    def get_rows(self, **cols) -> list:
         # Get a list of rows that matches with the cols
         if all(x in self.columns for x in list(cols)):
             return list(filter(lambda x:
@@ -107,7 +148,7 @@ class Table:
         else:
             raise ColumnError(f'All cols must be {self.name} columns')
 
-    def get_first(self, **cols):
+    def get_first(self, **cols) -> Row:
         # get the first element that matches with the cols
         rws = self.get_rows(**cols)
         return rws[0] if len(rws) else None
@@ -125,11 +166,14 @@ class Table:
             return Row(self, col_id)
         else:
             raise ColumnError(f'All cols must be {self.name} columns')
+
+    def remove_row(self, row_id: str):
+        self.d_rows = del_key(self.d_rows, row_id)
     
-    def add_column(self, cname, ctype):
+    def add_column(self, cname: str, ctype):
         self.columns = set_key(self.columns, cname, ctype)
 
-    def remove_column(self, cname):
+    def remove_column(self, cname: str):
         self.columns = del_key(self.columns, cname)
 
     def delete(self):
@@ -140,7 +184,7 @@ class Client(object):
     def __init__(self, filename="data.jldb"):
         self.interpreter = Interpreter(filename=filename)
 
-    def add_table(self, table_name: str, **columns):
+    def add_table(self, table_name: str, **columns) -> Table:
         """
             add a table with a unique table_name and columns with their respective types
             It's highly recommended to use built-in types
@@ -167,12 +211,12 @@ class Client(object):
         del table_dict[table_id]
         self.interpreter.update(table_dict)
 
-    def get_table_dict(self, table_id: str):
+    def get_table_dict(self, table_id: str) -> dict:
         # Gets a dict for this table
         return self.interpreter.read().get(table_id)
 
     @property
-    def tables(self):
+    def tables(self) -> list:
         # Gets a list of tables in this database 
         table_dict = self.interpreter.read()
         return list(map(lambda x: Table(self, x), table_dict))
@@ -187,44 +231,6 @@ class Client(object):
         new_tables_dict = self.interpreter.read()
         new_tables_dict[table_id] = new_dict
         self.interpreter.update(new_tables_dict)
-
-class Row(object):
-    def __init__(self, table, row_id: int):
-        super().__setattr__('table', table)
-        super().__setattr__('row_id', row_id)
-        super().__setattr__('_Row__column_types', self.table.columns)
-
-    @property
-    def __rowdict(self):
-        return self.table.d_rows[self.row_id]
-
-    def __setattr__(self, name, value):
-        if self.__column_types.get(name) != None:
-            r_dict = self.__rowdict
-            r_class = self.__column_types[name]
-            r_dict[name] = c_confirm(r_class, value)
-            self.table.d_rows = set_key(self.table.d_rows, self.row_id, r_dict)
-        else:
-            return super().__setattr__(name, value)
-
-    def __getattr__(self, name):
-        if self.__rowdict.get(name) != None:
-            return c_confirm(self.__column_types[name], self.__rowdict.get(name), raw=False)
-
-    def __delattr__(self, name):
-        if self.__rowdict.get(name) != None:
-            self.table.d_rows = set_key(self.table.d_rows, self.row_id, del_key(self.__rowdict, name))
-        else:
-            return super().__delattr__(name)
-
-    @property
-    def dict(self):
-        pdict = self.__dict__
-        for key in self.__rowdict:
-            value = self.__rowdict.get(key)
-            clss = self.__column_types.get(key)
-            pdict[key] = c_confirm(clss, value, raw=False)
-        return pdict
 
 if __name__ == "__main__":
     print("U should close that program huh?")
