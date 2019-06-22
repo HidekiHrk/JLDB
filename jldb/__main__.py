7b8039d1874458bb18a7546a8605d836b498bc9f
HidekiHrk <hidekihiroki123@gmail.com>
2019-06-22 18:19:34 -0300

    Added a shell to access a table

--- jldb/__main__.py
+++ jldb/__main__.py
@@ -16,14 +16,19 @@ def main(path):
     client = Client(path)
     exitStatus = 0
     current_table = None
-    alter = ""
+    current_row = None
+    def getstr():
+        nstr = {"current_row":current_row, "current_table":current_table, "client":client}
+        for x in nstr:
+            if nstr.get(x):
+                return x
     def clear():
         os.system("clear")
         print(
             f"JLDB Shell {version}",
             f"Editing JL Database file: {path}",
             f"Current Table: {current_table.name if current_table else 'Nenhuma'}",
-            f"Alterations: {alter}",
+            f"Current Row: {current_row.row_id if current_row else 'Nenhuma'}",
             sep="\n"
         )
     clear()
@@ -37,31 +42,55 @@ def main(path):
                 exitStatus += 1
                 continue
             elif exitStatus == 1:
-                if current_table:
+                if current_row:
+                    current_row = None
+                    exitStatus = 0
                     clear()
-                    message(f"Table {current_table.name} closed", linebreak=False)
+                    message(f"Row {current_row.row_id} closed", linebreak=False)
+                    continue
+                elif current_table:
                     current_table = None
                     exitStatus = 0
+                    clear()
+                    message(f"Table {current_table.name} closed", linebreak=False)
                     continue
-                sys.exit()
+                sys.exit(0)
         if exitStatus > 0: exitStatus = 0
         if command.strip().lower() == "exit":
+            if current_row: current_row = None; continue
+            if current_table: current_table = None; continue
+            sys.exit(0)
+        elif command.strip().lower() == "fexit":
             sys.exit(0)
         elif command.strip().lower() == "clear":
             clear()
-        elif command.strip().lower().startswith("table:"):
-            current_table = client.get_table(command.split(':'))
         else:
-            loc = locals()
-            obj_str = "current_table" if current_table else "client"
-            exec(f"result = {obj_str}.{command.strip()}", globals(), loc)
-            result = loc['result']
-            if type(result).__name__ == "Table":
-                current_table = result
-                clear()
-            elif type(result).__name__ == "Row":
-                print(result.dict)
-
+            if command.strip().lower().startswith("get:"):
+                new_command = command.strip()[4:]
+                if new_command:
+                    try:
+                        result = eval(f"{getstr()}.{new_command.strip()}")
+                    except Exception as e:
+                        message(f"Comando inválido.\n{e}", linebreak=False)
+                        continue
+                    if type(result).__name__ == "Row":
+                        current_row = result
+                        clear()
+                        continue
+                    if type(result).__name__ == "Table":
+                        current_table = result
+                        clear()
+                        continue
+                    message(f"Output: {result}", linebreak=False)
+                else:
+                    message(f"Comando inválido.", linebreak=False)
+                    continue
+            else:
+                try:
+                    exec(f"{getstr()}.{command.strip()}")
+                except Exception as e:
+                    message(f"Comando inválido.\n{e}", linebreak=False)
+                    continue
 args = sys.argv
 
 if len(args) >= 2:
